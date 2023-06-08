c60d4b21c5f9bbce505d7aa8fb005b17442f835c
Flávio Araújo <flavioafa@gmail.com>
2023-06-08 20:07:30 -0300

    Object-Oriented Principles in PHP - Seção 1 - Aula Objects - Modificado o arquivo index com duas classes pra mostrar exemplos de instancia de objetos;

--- index.php
+++ index.php
@@ -1,13 +1,62 @@
 <?php
 
-class ArchievementBadges 
+class Team 
 {
-    public $title;
-    public $description;
-    public $points;
+    protected $name;
+    protected $members = [];
 
-    public function awardTo($user)
+    public function __construct($name, $members = [])
+    {
+        $this->name = $name;
+        $this->members = $members;
+    }
+
+    public static function start(...$params)
+    {
+        return new static(...$params);
+    }
+
+    public function name()
+    {
+        return $this->name;
+    }
+
+    public function members()
+    {
+        return $this->members;
+    }
+
+    public function add($name)
+    {
+        $this->members[] = $name;
+    }
+
+    public function cancel()
+    {
+
+    }
+
+    public function manager()
     {
 
     }
 }
+
+class Member
+{
+    protected $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+}
+
+$acme = Team::start('Acme', [
+    new Member('John Doe'),
+    new Member('Jane Doe')
+]);
+
+$acme->add('Frank Doe');
+
+var_dump($acme->members());
